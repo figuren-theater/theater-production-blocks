33c926720336f837944230e6c168badd4e785bfd
Carsten Bach <mail@carsten-bach.de>
2023-09-27 15:49:55 +0200

    Cleanup

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Register the theater-production-blocks module to altis
+ * Load the theater-production-blocks WordPress plugin
  *
  * @package           figuren-theater/theater-production-blocks
  * @author            figuren.theater
@@ -8,23 +8,25 @@
  * @license           GPL-3.0+
  *
  * @wordpress-plugin
- * Plugin Name:       figuren.theater | theater_production_blocks
+ * Plugin Name:       Theater Production Blocks
  * Plugin URI:        https://github.com/figuren-theater/theater-production-blocks
- * Description:       ... like the figuren.theater WordPress Multisite network.
+ * Description:       WordPress blocks for theater productions to be used with the Site- and Block-Editor.
  * Version:           0.1.0-alpha
  * Requires at least: 6.0
  * Requires PHP:      7.1
  * Author:            figuren.theater
  * Author URI:        https://figuren.theater
- * Text Domain:       figurentheater
+ * Text Domain:       theater-production-blocks
  * Domain Path:       /languages
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Update URI:        https://github.com/figuren-theater/theater-production-blocks
  */
 
-namespace Figuren_Theater\theater_production_blocks;
+namespace Figuren_Theater\Production_Blocks;
+
+use function add_action;
 
 const DIRECTORY = __DIR__;
 
-add_action( 'altis.modules.init', __NAMESPACE__ . '\\register' );
+add_action( 'init', __NAMESPACE__ . '\\register', 0 );
