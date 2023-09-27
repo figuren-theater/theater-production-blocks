bf838230cdf099d9336fc8151d772606f4851fcb
carstingaxion <carstingaxion@users.noreply.github.com>
2023-09-27 10:43:52 +0000

    ✅ Ready to clone and code.

--- inc/class-example.php
+++ inc/class-example.php
@@ -2,15 +2,15 @@
 /**
  * Figuren_Theater ...
  *
- * @package figuren-theater/project_urlname
+ * @package figuren-theater/theater-production-blocks
  */
 
-namespace Figuren_Theater\project_name;
+namespace Figuren_Theater\theater_production_blocks;
 
 /**
  * Figuren_Theater ...
  *
- * @package figuren-theater/project_urlname
+ * @package figuren-theater/theater-production-blocks
  */
 class Example {
 
--- inc/namespace.php
+++ inc/namespace.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Figuren_Theater project_name.
+ * Figuren_Theater theater_production_blocks.
  *
- * @package figuren-theater/project_urlname
+ * @package figuren-theater/theater-production-blocks
  */
 
-namespace Figuren_Theater\project_name;
+namespace Figuren_Theater\theater_production_blocks;
 
 use Altis;
 
@@ -24,9 +24,9 @@ function register() :void {
 	];
 
 	Altis\register_module(
-		'project_urlname',
+		'theater-production-blocks',
 		DIRECTORY,
-		'project_name',
+		'theater_production_blocks',
 		$options,
 		__NAMESPACE__ . '\\bootstrap'
 	);
