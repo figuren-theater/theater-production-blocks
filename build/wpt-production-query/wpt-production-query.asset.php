65460e0102b520fb763e31461ed4efb796fe5f3d
Carsten Bach <mail@carsten-bach.de>
2024-02-19 22:49:45 +0100

    Build

--- build/wpt-production-query/wpt-production-query.asset.php
+++ build/wpt-production-query/wpt-production-query.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9ba57831237bade33567');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '09ae2941749c746657db');
