bdf871c5545697c64dcb0f49aebc911f36b638fa
Carsten Bach <mail@carsten-bach.de>
2024-02-19 03:22:49 +0100

    Cleanup

--- inc/registration/namespace.php
+++ inc/registration/namespace.php
@@ -84,7 +84,6 @@ function get_post_type_supports( string $post_type ): array {
 			'premiere',
 			'duration',
 			'targetgroup',
-			// 'wpt-production-query', // Only WIP and shouldn't be here !!
 		],
 		$post_type
 	);
--- src/block-editor/blocks/wpt-production-query/namespace.php
+++ src/block-editor/blocks/wpt-production-query/namespace.php
@@ -2,7 +2,6 @@
 /**
  * We are on INIT:9
  */
-// wp_die( 'huh' );
 
 /**
  * Fires after WordPress has finished loading but before any headers are sent.
@@ -10,8 +9,6 @@
 \add_action(
 	'init', 
 	function (): void {
-        // wp_die( var_export( \register_block_type_from_metadata( __DIR__ ) ) );
-		// \register_block_type( __DIR__ );
         \register_block_type_from_metadata( __DIR__ );
 	} 
 );
